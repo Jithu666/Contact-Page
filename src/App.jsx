35ad6aaf16032b9feaf070d57aac2d199b7fc4b8
githendragowdakr@gmail.com <githendragowdakr@gmail.com>
2025-09-10 13:06:49 +0530

    Added svg's into assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <>
-      <h1>Contact us page</h1>
-    </>
+    <div className="flex flex-col">
+      {/* <h1>Contact us page</h1> */}
+      <Header />
+      <Hero />
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import HeaderLogo from "../assets/headerLogo.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <img src={HeaderLogo} alt="Header Logo" className="px py-12" />
+    <div className="flex flex-col justify-center">
+      <Link to="/">
+        <img
+          src={HeaderLogo}
+          alt="Header Logo"
+          className="flex items-start w-16 ml-14 cursor-pointer"
+        />
+      </Link>
     </div>
   );
 };
